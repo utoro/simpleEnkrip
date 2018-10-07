fdd527940d341b0664f9000f596c7f65061c7718
Utoro <utoro.ardiyatno@gmail.com>
2018-10-07 21:54:32 +0700

    initial commit

---
+++ simpleEnkrip.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+from itertools import cycle
+import argparse
+
+
+def scramble(data, key):
+    result = ''.join(chr(ord(c) ^ ord(k)) for c, k in zip(data, cycle(key)))
+    return result
+
+
+def main():
+    ap = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='Simple Enkrip Dekrip Program'
+    )
+
+    ap.add_argument('-k', '--key', default='R4h4514', help='Key untuk enkripsi')
+    ap.add_argument('-i', '--input', required=True, help='Nama file input')
+    ap.add_argument('-o', '--output', help='Nama file output', default='output.txt')
+    ap.add_argument('-p', '--print', action='store_true', default=False, help='Key untuk enkripsi')
+    args = vars(ap.parse_args())
+
+    if args['print']:
+        print(scramble(args['input'], args['key']))
+    else:
+        # data = open(args['input'], mode='r', encoding='utf-8').read()
+        try:
+            data = open(args['input'], mode='r', encoding='utf-8').read()
+        except FileNotFoundError:
+            print('File tidak ditemukan')
+            argparse._sys.exit(1)
+        except:
+            print('Gagal!\nMaaf file tidak didukung')
+            argparse._sys.exit(1)
+
+        task = scramble(data, args['key'])
+        save = open(args['output'], mode='w', encoding='utf-8')
+        save.write(task)
+        save.close()
+
+
+if __name__ == '__main__':
+    main()
